6b298b295323ef50eae14eeae6617d2157642fb2
Mohith-Kumar28 <83702382+Mohith-Kumar28@users.noreply.github.com>
2022-08-13 16:35:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,74 +24,74 @@ process.on("uncaughtException",(err)=>{
 });
 
 
-// connectDatabase();
+connectDatabase();
 
-// cloudinary.config({ 
-//     cloud_name:process.env.CLOUDINARY_NAME,
-//     api_key:process.env.CLOUDINARY_API_KEY,
-//     api_secret:process.env.CLOUDINARY_API_SECRET
-// });
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
 
 const server=app.listen(process.env.PORT || 4000,async()=>{
     try {
      
-//         watchOrder=orderSchema.watch(
-//             [
-//             {
+        watchOrder=orderSchema.watch(
+            [
+            {
   
-//                 "$match":{
+                "$match":{
             
-//                     operationType:"update",
-//                     "fullDocument.orderStatus":"initiated",
-//                     "fullDocument.paymentInfo.status":"paid"
-//                 }
-//             }
-//         ] 
-//         ,{"fullDocument":"updateLookup"} 
-//          )
+                    operationType:"update",
+                    "fullDocument.orderStatus":"initiated",
+                    "fullDocument.paymentInfo.status":"paid"
+                }
+            }
+        ] 
+        ,{"fullDocument":"updateLookup"} 
+         )
 
 
 
 
 
-//          watchOrder.on("change",(next)=>{
+         watchOrder.on("change",(next)=>{
 
-//             const thisShopId=next.fullDocument?.shop?.toString()
-//             const newOrder=next.fullDocument
+            const thisShopId=next.fullDocument?.shop?.toString()
+            const newOrder=next.fullDocument
            
        
-//             io.to(thisShopId).emit("newOrder",newOrder)
+            io.to(thisShopId).emit("newOrder",newOrder)
 
 
-//             const beamsClient = new PushNotifications({
-//                 instanceId: process.env.PUSHER_INSTANCE_ID,
-//                 secretKey: process.env.PUSHER_SECRET_ID,
-//               });
-//               try {
+            const beamsClient = new PushNotifications({
+                instanceId: process.env.PUSHER_INSTANCE_ID,
+                secretKey: process.env.PUSHER_SECRET_ID,
+              });
+              try {
                   
              
-//             beamsClient.publishToUsers([newOrder.shopOwner?.toString()], {
+            beamsClient.publishToUsers([newOrder.shopOwner?.toString()], {
   
-//     web: {
-//       notification: {
-//         title: `New order - ₹${newOrder.totalPrice}`, 
-//         body:`${ newOrder.orderItems.map((ord)=> ord.name+"x"+ord.quantity+", ")}`,
-//         deep_link: `${process.env.FRONTEND_HOST}/${newOrder.shop}/admin/orders`,
-//         icon:"https://picsum.photos/200"
-//       },
-//     },
-//   })
-//   .then((publishResponse) => {
-//     // console.log("Just published:", publishResponse.publishId);
-//   })
-//   .catch((error) => {
-//     // console.error("Error:", error);
-//   });
-// } catch (error) {
-//          console.log(error)         
-// }     
+    web: {
+      notification: {
+        title: `New order - ₹${newOrder.totalPrice}`, 
+        body:`${ newOrder.orderItems.map((ord)=> ord.name+"x"+ord.quantity+", ")}`,
+        deep_link: `${process.env.FRONTEND_HOST}/${newOrder.shop}/admin/orders`,
+        icon:"https://picsum.photos/200"
+      },
+    },
+  })
+  .then((publishResponse) => {
+    // console.log("Just published:", publishResponse.publishId);
+  })
+  .catch((error) => {
+    // console.error("Error:", error);
+  });
+} catch (error) {
+         console.log(error)         
+}     
   
-//         })  
+        })  
         
         console.log(`Server is working on ${process.env.BACKEND_HOST}`)
         // console.log(`Server is working on http://localhost:${process.env.PORT}`)
@@ -117,27 +117,27 @@ process.on("unhandledRejection",(err)=>{
 
 
 // socket.io
-//  const io=require("socket.io")(server,{
-//     pingTimeout:60000,
-//     cors:{
-//         origin:process.env.FRONTEND_HOST
-//         // origin:'http://localhost:3000'
-//     }
+ const io=require("socket.io")(server,{
+    pingTimeout:60000,
+    cors:{
+        origin:process.env.FRONTEND_HOST
+        // origin:'http://localhost:3000'
+    }
 
-// })
-//     io.on("connection",(socket)=>{
+})
+    io.on("connection",(socket)=>{
  
-//         console.log("connected to socket.io");
+        console.log("connected to socket.io");
   
         
 
-//         socket.on("setup",(shopId)=>{
-//             socket.join(shopId);
-//             socket.activeRoom=shopId;
-//             console.log(shopId);
-//             // console.log(socket.rooms);  
-//             socket.emit("connected")
-//         })
+        socket.on("setup",(shopId)=>{
+            socket.join(shopId);
+            socket.activeRoom=shopId;
+            console.log(shopId);
+            // console.log(socket.rooms);  
+            socket.emit("connected")
+        })
  
     
-// }) 
+}) 
