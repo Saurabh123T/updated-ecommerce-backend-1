2bbced501bbad0e8f494bf4199f09e511cf70110
Mohith kumar <murugan28aug@gmail.com>
2022-08-15 20:33:19 +0530

    nothing

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ const server=app.listen(process.env.PORT || 4000,async()=>{
   
                 "$match":{
             
-                    operationType:"insert",
-                    // operationType:"update",
-                    // "fullDocument.orderStatus":"initiated",
-                    // "fullDocument.paymentInfo.status":"paid"
+                    // operationType:"insert",
+                    operationType:"update",
+                    "fullDocument.orderStatus":"initiated",
+                    "fullDocument.paymentInfo.status":"paid"
                 }
             }
         ] 
