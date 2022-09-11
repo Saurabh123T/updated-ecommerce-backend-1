f834861564023507eb2758ea7559e53b250726f3
Mohith kumar <murugan28aug@gmail.com>
2022-09-11 18:21:16 +0530

    added time log

--- controllers/paymentControllers/pretransaction.js.js
+++ controllers/paymentControllers/pretransaction.js.js
@@ -14,7 +14,7 @@ const date = require("date-and-time")
 exports.pretransaction=catchAsyncErrors(async(req,res,next)=>{
     const shop = await shopSchema.findById({_id:req.body.cartShop})
 
-    console.log(new Date())
+    console.log(date.format(new Date(), 'YYYY/MM/DD HH:mm:ss'))
     // const Mid=await shop.paymentMethods.paytmMid;
     // const Mkey=await shop.paymentMethods.paytmMkey;
 
