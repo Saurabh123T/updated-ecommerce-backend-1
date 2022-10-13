1dcb62a187cc0aa843e456b410d0dbc7e06a4892
Mohith kumar <murugan28aug@gmail.com>
2022-10-13 16:51:03 +0530

    commented

--- controllers/shopControllers/shopController.js
+++ controllers/shopControllers/shopController.js
@@ -16,24 +16,24 @@ const mongoose=require("mongoose")
 
 // create Shop 
 exports.createShop=catchAsyncErrors(async(req,res,next)=>{
-  try {
+//   try {
     
   
- const captchaRes= await axios.post(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.body.captchaToken}`
-    );
-console.log(captchaRes)
-if(!captchaRes.data.success){
-  return next(new ErrorHandler("Captcha validation failed", 404));
+//  const captchaRes= await axios.post(
+//     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.body.captchaToken}`
+//     );
+// console.log(captchaRes)
+// if(!captchaRes.data.success){
+//   return next(new ErrorHandler("Captcha validation failed", 404));
 
-}
+// }
  
 
-} catch (error) {
-  console.log("error",error)
-  return next(new ErrorHandler("Captcha validation failed", 404));
+// } catch (error) {
+//   console.log("error",error)
+//   return next(new ErrorHandler("Captcha validation failed", 404));
     
-}
+// }
  
  
 try {
